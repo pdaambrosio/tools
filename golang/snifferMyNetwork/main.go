f75f398680d2f7ce69f581633b229eebd0be4d67
pdaambrosio <pda.ambrosio@gmail.com>
2024-07-05 12:11:27 -0300

    feat: function main and readme

--- golang/snifferMyNetwork/main.go
+++ golang/snifferMyNetwork/main.go
@@ -6,10 +6,15 @@ import (
 	"github.com/pdaambrosio/tools/src"
 )
 
+// The main function prompts the user to enter a network interface name and BPF filter, then calls
+// functions to search for network interfaces and capture packets on the specified interface.
 func main() {
 	var deviceName string
+	var setFilter string
 	fmt.Print("Enter the network interface name: ")
 	fmt.Scanln(&deviceName)
+	fmt.Print("Enter the BPF filter: ")
+	fmt.Scanln(&setFilter)
 
 	// Call the function `FindInterfaces` from the package `src` to search for a network interface
 	fmt.Println("Searching for network interfaces...")
@@ -19,5 +24,5 @@ func main() {
 
 	// Call the function `LiveCapture` from the package `src` to capture packets on the network interface
 	fmt.Println("Capturing packets...")
-	src.LiveCapture(deviceName, "tcp and port 80")
+	src.LiveCapture(deviceName, setFilter)
 }
