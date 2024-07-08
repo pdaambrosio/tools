4e0092cbe7cfe15b34ad933b3031d2aa405262c8
pdaambrosio <pda.ambrosio@gmail.com>
2024-07-08 20:35:40 -0300

    feat: sync and waitgroup

--- golang/portScanner/src/scanner.go
+++ golang/portScanner/src/scanner.go
@@ -3,23 +3,30 @@ package src
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 // The ScanPorts function scans a range of ports on a given IP address to check for open ports.
 func ScanPorts(ip string, portRange int) {
+	var wg sync.WaitGroup
 	fmt.Println("Scanning IP: ", ip)
 
 	for i := 1; i <= portRange; i++ {
-		address := fmt.Sprintf("%s:%d", ip, i)
-		conn, err := net.Dial("tcp", address)
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			address := fmt.Sprintf("%s:%d", ip, j)
+			conn, err := net.Dial("tcp", address)
 
-		if err != nil {
-			// Port is closed
-			fmt.Println("Port closed: ", i)
-			continue
-		}
+			if err != nil {
+				// Port is closed
+				fmt.Println("Port is closed or filtered: ", j)
+				return
+			}
 
-		conn.Close()
-		fmt.Printf("%d Open\n", i)
+			conn.Close()
+			fmt.Printf("%d Open\n", j)
+		}(i)
 	}
+	wg.Wait()
 }
